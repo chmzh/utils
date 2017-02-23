74969fc4a96b750d3ee71390020a159784883c13
chenmingzhou <sd@dfh.com>
2017-02-23 17:24:25 +0800

    rand access file

---
+++ src/main/java/com/cmz/nio/raf/Position.java
@@ -0,0 +1,47 @@
+package com.cmz.nio.raf;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.EnumSet;
+
+public class Position {
+	public static void main(String[] args) {
+		  Path path = Paths.get(System.getProperty("user.dir"), "story.txt");
+		  ByteBuffer buffer = ByteBuffer.allocate(1);
+		  String encoding = System.getProperty("file.encoding");
+		  try (SeekableByteChannel seekableByteChannel = (Files.newByteChannel(path,
+		                                                      EnumSet.of(StandardOpenOption.READ)))) {
+		   //the initial position should be 0 anyway
+		   seekableByteChannel.position(0);
+		   System.out.println("Reading one character from position: " +
+		                                                              seekableByteChannel.position());
+		   seekableByteChannel.read(buffer);
+		   buffer.flip();
+		   System.out.print(Charset.forName(encoding).decode(buffer));
+		   buffer.rewind();
+		   //get into the middle
+		   seekableByteChannel.position(seekableByteChannel.size()/2);
+		   System.out.println("\nReading one character from position: " +
+		                                                              seekableByteChannel.position());
+		   seekableByteChannel.read(buffer);
+		   buffer.flip();
+		   System.out.print(Charset.forName(encoding).decode(buffer));
+		   buffer.rewind();
+		   //get to the end
+		   seekableByteChannel.position(seekableByteChannel.size()-1);
+		   System.out.println("\nReading one character from position: " +
+		                                                              seekableByteChannel.position());
+		   seekableByteChannel.read(buffer);
+		   buffer.flip();
+		   System.out.print(Charset.forName(encoding).decode(buffer));
+		   buffer.clear();
+		  } catch (IOException ex) {
+		   System.err.println(ex);
+		} }
+}
