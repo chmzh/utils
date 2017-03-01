b1bf374ca6ec9a4527b1373bde86978fd9bb6578
chenmingzhou <sd@dfh.com>
2017-03-01 16:57:46 +0800

    spliterator

---
+++ src/main/java/com/cmz/spliterator/ArraysTest.java
@@ -0,0 +1,12 @@
+package com.cmz.spliterator;
+
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+
+public class ArraysTest {
+	public static void main(String[] args) {
+		double[] array = {1,2,3,4,5};
+		double d = StreamSupport.doubleStream(Arrays.spliterator(array), false).sum();
+		System.out.println(d);
+	}
+}
