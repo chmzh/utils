20e83797aa7c11d0b42794d6553614fab893a588
chenmingzhou <sd@dfh.com>
2017-03-01 17:57:11 +0800

    stringjoiner

---
+++ src/main/java/com/cmz/string/Test.java
@@ -0,0 +1,21 @@
+package com.cmz.string;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Test {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+		 String commaSeparatedNumbers = numbers.stream()
+		     .map(i -> i.toString())
+		     .collect(Collectors.joining(", "));
+		 System.out.println(commaSeparatedNumbers);
+		 
+		 StringJoiner sj = new StringJoiner(":", "[", "]");
+		 sj.add("George").add("Sally").add("Fred");
+		 String desiredString = sj.toString();
+		 System.out.println(desiredString);
+	}
+}
