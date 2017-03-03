f8bfb8a3b76bd529b16568675257c08fad3078d0
chenmingzhou <sd@dfh.com>
2017-03-03 11:08:42 +0800

    nio

---
+++ src/main/java/com/cmz/nio/attr/ACLView.java
@@ -0,0 +1,5 @@
+package com.cmz.nio.attr;
+
+public class ACLView {
+
+}
--- src/main/java/com/cmz/nio/attr/POSIXView.java
+++ src/main/java/com/cmz/nio/attr/POSIXView.java
@@ -3,14 +3,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.GroupPrincipal;
 import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 public class POSIXView {
 	public static void main(String[] args) {
-		read();
+		setGroup();
 	}
 	
 	private static void read(){
@@ -38,10 +40,30 @@ public class POSIXView {
 	}
 	
 	private static void set(){
-		Path new_path = Paths.get("story.txt");
-		Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rw-r--r--");
+		Path new_path = Paths.get("story4.txt");
+		Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rwxr--r--");
 		try {
 		    Files.setPosixFilePermissions(new_path, permissions);
+		} catch (IOException e) {
+		    System.err.println(e);
+		}
+	}
+	
+	private static void setGroup(){
+		Path path = Paths.get("story4.txt");
+		
+		try {
+		    GroupPrincipal group = path.getFileSystem().
+		               getUserPrincipalLookupService().lookupPrincipalByGroupName("chenmingzhou");
+		    Files.getFileAttributeView(path, PosixFileAttributeView.class).setGroup(group);
+		} catch (IOException e) {
+		    System.err.println(e);
+		}
+
+		try {
+		    GroupPrincipal group = (GroupPrincipal) Files.getAttribute(path, "posix:group",
+		                                                                       NOFOLLOW_LINKS);
+		    System.out.println(group.getName());
 		} catch (IOException e) {
 		    System.err.println(e);
 		}
