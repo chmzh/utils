392bf99dcbddc8b420c1e30349de0404951b3fd2
chenmingzhou <sd@dfh.com>
2017-03-03 12:28:51 +0800

    nio

--- src/main/java/com/cmz/nio/fileanddir/DirUtil.java
+++ src/main/java/com/cmz/nio/fileanddir/DirUtil.java
@@ -1,11 +1,17 @@
 package com.cmz.nio.fileanddir;
 
+import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
 
 public class DirUtil {
 	public static void main(String[] args) {
-		test1();
+		test2();
 	}
 	/**
 	 * Listing File System Root Directories
@@ -14,6 +20,20 @@ public class DirUtil {
 		Iterable<Path> dirs = FileSystems.getDefault().getRootDirectories();
 		for (Path name : dirs) {
 		     System.out.println(name);
+		}
+	}
+	
+	/**
+	 * Creating a New Directory
+	 */
+	private static void test2(){
+		Path newdir = FileSystems.getDefault().getPath("test");
+		try {
+			Set<PosixFilePermission> perms = PosixFilePermissions.fromString("rwxr-x---");
+			FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(perms);
+		    Files.createDirectory(newdir,attr);
+		} catch (IOException e) {
+		    System.err.println(e);
 		}
 	}
 }
