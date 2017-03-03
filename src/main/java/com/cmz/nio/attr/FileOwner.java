1d43500b4d0dc02389939d4300c8eb6cacba5434
chenmingzhou <sd@dfh.com>
2017-03-03 10:57:25 +0800

    nio

--- src/main/java/com/cmz/nio/attr/FileOwner.java
+++ src/main/java/com/cmz/nio/attr/FileOwner.java
@@ -6,13 +6,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.UserPrincipal;
-
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 public class FileOwner {
 	public static void main(String[] args) {
-		test2();
+		test5();
 	}
 	
-	//Set a File Owner Using Files.setOwner()
+	/**
+	 * Set a File Owner Using Files.setOwner()
+	 */
 	private static void test1(){
 		UserPrincipal owner = null;
 		Path path = Paths.get("story.txt");
@@ -24,7 +26,9 @@ public class FileOwner {
 		}
 	}
 	
-	//Set a File Owner Using FileOwnerAttributeView.setOwner()
+	/**
+	 * Set a File Owner Using FileOwnerAttributeView.setOwner()
+	 */
 	private static void test2(){
 		UserPrincipal owner = null;
 		Path path = Paths.get("story.txt");
@@ -36,5 +40,45 @@ public class FileOwner {
 		} catch (IOException e) {
 		    System.err.println(e);
 		}
+	}
+	
+	private static void test3(){
+		UserPrincipal owner = null;
+		Path path = Paths.get("story.txt");
+		try {
+		    owner = path.getFileSystem().getUserPrincipalLookupService().
+		                                 lookupPrincipalByName("apress");
+		    Files.setAttribute(path, "owner:owner", owner, NOFOLLOW_LINKS);
+		} catch (IOException e) {
+		    System.err.println(e);
+		}
+	}
+	/**
+	 * Get a File Owner Using FileOwnerAttributeView.getOwner()
+	 */
+	private static void test4(){
+		Path path = Paths.get("story.txt");
+		FileOwnerAttributeView foav = Files.getFileAttributeView(path,
+		                                        FileOwnerAttributeView.class);
+		try {
+		    String owner = foav.getOwner().getName();
+		    System.out.println(owner);
+		} catch (IOException e) {
+		    System.err.println(e);
+		}
+	}
+	
+	/**
+	 * Get a File Owner Using Files.getAttribute()
+	 */
+	private static void test5(){
+		Path path = Paths.get("story.txt");
+		try {
+		    UserPrincipal owner = (UserPrincipal) Files.getAttribute(path,
+		                                               "owner:owner", NOFOLLOW_LINKS);
+		    System.out.println(owner.getName());
+		    } catch (IOException e) {
+		        System.err.println(e);
+		    }
 	}
 }
