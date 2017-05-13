36e59b1b0a2894a21fa4a2d863b54382da843ca2
chmzh <28458942@qq.com>
2017-05-13 12:27:49 +0800

    Update StreamTest.java

--- src/main/java/com/lambda/StreamTest.java
+++ src/main/java/com/lambda/StreamTest.java
@@ -20,15 +20,34 @@ public class StreamTest {
         list.add(TestEntry.valueOf("1","0","3","0"));
         list.add(TestEntry.valueOf("1","0","3","0"));
 
-        Map<String,Map<String,List<TestEntry>>> map;
-        map = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getTwo)));  //1，2 位相同
-        map = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getThree)));  //1，3 位相同
-        map = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getFour)));  //1，4 位相同
-        map.forEach((key,value)->{
+        Map<String,Map<String,List<TestEntry>>> map2;
+        map2 = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getTwo)));  //1，2 位相同
+        map2 = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getThree)));  //1，3 位相同
+        map2 = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getFour)));  //1，4 位相同
+        map2 = list.stream().collect(Collectors.groupingBy(TestEntry::getTwo,Collectors.groupingBy(TestEntry::getThree)));  //2，3 位相同
+        map2 = list.stream().collect(Collectors.groupingBy(TestEntry::getTwo,Collectors.groupingBy(TestEntry::getFour)));  //2，4 位相同
+        map2 = list.stream().collect(Collectors.groupingBy(TestEntry::getThree,Collectors.groupingBy(TestEntry::getFour)));  //3，4 位相同
+
+
+        Map<String,Map<String,Map<String,List<TestEntry>>>> map3;
+        map3 = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getTwo,Collectors.groupingBy(TestEntry::getThree))));  //1，2,3 位相同
+        map3 = list.stream().collect(Collectors.groupingBy(TestEntry::getOne,Collectors.groupingBy(TestEntry::getTwo,Collectors.groupingBy(TestEntry::getFour))));  //1，2,4 位相同
+        map3 = list.stream().collect(Collectors.groupingBy(TestEntry::getTwo,Collectors.groupingBy(TestEntry::getThree,Collectors.groupingBy(TestEntry::getFour))));  //2，3,4 位相同
+        //map2
+        map2.forEach((key,value)->{
             value.forEach((key2,value2)->{
                 if(value2.size()>1){
                     System.out.println(key2+"=>"+value2);
                 }
+            });
+
+        });
+        //map3
+        map3.forEach((key,value)->{
+            value.forEach((key2,value2)->{
+                value2.forEach((key3,value3) ->{
+
+                });
             });
 
         });
